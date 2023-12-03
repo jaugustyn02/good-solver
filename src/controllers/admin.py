87a17441627ddfbff28873120ae19d6575cb27b3
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2023-12-03 20:26:59 +0100

    admin.py: handling alternatives

--- src/controllers/admin.py
+++ src/controllers/admin.py
@@ -1,13 +1,14 @@
 from flask import render_template, request, flash, redirect, url_for
 from mysql.connector import connect
 from models.experts import Expert, get_experts, create_expert, delete_expert
+from models.alternatives import Alternative, get_alternatives, create_alternative, delete_alternative
 
 
 def configure_admin_routes(app):
     @app.route('/admin', methods=['GET', 'POST'])
     def admin():
         if request.method == 'GET':
-            return render_template('admin.html', experts=get_experts())
+            return render_template('admin.html', experts=get_experts(), alternatives=get_alternatives())
         elif request.method == 'POST':
             action = request.form.get('action')
             
@@ -15,7 +16,11 @@ def configure_admin_routes(app):
                 return handle_add_expert()
             elif action == 'delete_expert':
                 return handle_delete_expert()
-        return render_template('admin.html', experts=get_experts())
+            elif action == 'add_alternative':
+                return handle_add_alternative()
+            elif action == 'delete_alternative':
+                return handle_delete_alternative()
+        return render_template('admin.html', experts=get_experts(), alternatives=get_alternatives())
 
     def handle_add_expert():
         name, mail = request.form['name'], request.form['mail']
@@ -35,5 +40,24 @@ def configure_admin_routes(app):
             flash('ID is required!')
         else:
             result = delete_expert(int(expert_id))
+            flash(result.message)
+        return redirect(url_for('admin'))
+    
+    def handle_add_alternative():
+        name, description = request.form['name'], request.form['description']
+        if not name:
+            flash('Name is required!')
+        else:
+            alternative = Alternative(name, description)
+            result = create_alternative(alternative)
+            flash(result.message)
+        return redirect(url_for('admin'))
+    
+    def handle_delete_alternative():
+        alternative_id = request.form['id']
+        if not alternative_id:
+            flash('ID is required!')
+        else:
+            result = delete_alternative(int(alternative_id))
             flash(result.message)
         return redirect(url_for('admin'))
