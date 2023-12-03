4d1bc1b81c9d5518f619563904c3f7dc9b46bbf2
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2023-12-03 18:27:36 +0100

    delete expert

--- src/controllers/admin.py
+++ src/controllers/admin.py
@@ -1,21 +1,42 @@
-from flask import render_template, request, flash
+from flask import render_template, request, flash, redirect, url_for
 from mysql.connector import connect
-from models.experts import get_experts, create_expert, Expert
+from models.experts import Expert, get_experts, create_expert, delete_expert
 
 
 def configure_admin_routes(app):
-    # create expert
     @app.route('/admin', methods=['GET', 'POST'])
     def admin():
-        if request.method == 'POST':
-            name, mail = request.form['name'], request.form['mail']
-            if not name:
-                flash('Name is required')
-            elif not mail:
-                flash('Mail is required')
-            else:
-                expert = Expert(name, mail)
-                create_expert(expert)
-                flash('Expert created successfully')
-                
+        if request.method == 'GET':
+            return render_template('admin.html', experts=get_experts())
+        elif request.method == 'POST':
+            action = request.form.get('action')
+            
+            if action == 'add_expert':
+                return handle_add_expert()
+            elif action == 'delete_expert':
+                return handle_delete_expert()
         return render_template('admin.html', experts=get_experts())
+
+    def handle_add_expert():
+        name, mail = request.form['name'], request.form['mail']
+        if not name:
+            flash('Name is required!')
+        elif not mail:
+            flash('Mail is required!')
+        else:
+            expert = Expert(name, mail)
+            create_expert(expert)
+            flash('Expert created successfully')
+                
+        return redirect(url_for('admin'))
+    
+    def handle_delete_expert():
+        expert_id = request.form['id']
+        print('EXPERT ID:', expert_id)
+        if not id:
+            flash('ID is required!')
+        else:
+            delete_expert(int(expert_id))
+            flash('Expert deleted successfully')
+            
+        return redirect(url_for('admin'))
--- src/models/experts.py
+++ src/models/experts.py
@@ -5,8 +5,11 @@ class Expert:
         self.id = id
         self.name = name
         self.mail = mail
+    
+    def __str__(self):
+        return f'Expert(id={self.id}, name={self.name}, mail={self.mail})'
 
-def get_experts():
+def get_experts() -> list:
     experts = []
     db = get_db()
     cursor = db.cursor()
@@ -17,10 +20,18 @@ def get_experts():
     db.close()
     return experts
 
-def create_expert(expert: Expert):
+def create_expert(expert: Expert) -> None:
     db = get_db()
     cursor = db.cursor()
     cursor.execute('INSERT INTO Experts (name, address) VALUES (%s, %s)', (expert.name, expert.mail))
+    db.commit()
+    cursor.close()
+    db.close()
+    
+def delete_expert(expert_id: int) -> None:
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('DELETE FROM Experts WHERE expert_id = %s', (expert_id,))
     db.commit()
     cursor.close()
     db.close()
