72d0cfb4239a22a313ee68af24f6b51b7980a821
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2024-01-19 19:16:47 +0100

    .env

--- src/config.py
+++ src/config.py
@@ -1,14 +1,20 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 class Config:
     DEBUG = False
     TESTING = False
     SECRET_KEY = b'r6\xb5\xd9K\xd5\t\x84X}\x16\x88\xf0t^\x15\xaf\xadn)\xca\xf7\xd4k'
-    MYSQL_HOST = 'mysql.agh.edu.pl'
-    MYSQL_USER = 'jaugusty'
-    MYSQL_PASSWORD = 'jbJPBNJmrWipaigs'
-    MYSQL_DB = 'jaugusty'
-    MYSQL_PORT = 3306
+    MYSQL_HOST = os.environ.get('HOST')
+    MYSQL_USER = os.environ.get('USER_NAME')
+    MYSQL_PASSWORD = os.environ.get('PASSWORD')
+    MYSQL_DB = os.environ.get('DB')
+    MYSQL_PORT = os.environ.get('PORT')
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
